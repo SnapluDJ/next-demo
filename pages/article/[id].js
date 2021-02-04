3d2d38cad42c6145f03d5c0fb0b99ebdeb2440ce
Dongjie <dongjielu@wustl.edu>
2021-02-04 23:09:30 +0800

    date fetching

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -6,10 +6,18 @@ const article = (props) => {
 
   const router = useRouter();
 
+  if (router.isFallback) {
+    return <div>loading</div>;
+  }
+
   return (
     <div>
       <div>this is article {router.query.id}</div>
       <p>{article.body}</p>
+
+      <Link href="/">go back</Link>
+
+      <br />
 
       <Link href={`/article/${router.query.id}/edit`}>
         <a>click to edit</a>
@@ -18,7 +26,19 @@ const article = (props) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: { article },
+//   };
+// };
+
+export const getStaticProps = async (context) => {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
   );
@@ -27,6 +47,23 @@ export const getServerSideProps = async (context) => {
 
   return {
     props: { article },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+
+  const articles = await res.json();
+
+  return {
+    paths: articles.map((a) => `/article/${a.id}`),
+    // paths: articles.map((a) => ({
+    //   params: { id: a.id.toString() },
+    // })),
+    fallback: true,
+    // fallback: false // will show 404 not found
   };
 };
 
